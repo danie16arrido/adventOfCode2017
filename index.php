70789e239c5e480e5edead092977a58c61e96f98
danie16arrido <daniel.garrido.info@gmail.com>
2017-12-10 21:42:21 +0000

    add to index

--- index.php
+++ index.php
@@ -7,9 +7,13 @@ $reader = new \DayTwo\FileReader;
 
 $array = $reader->get2DArrayFromCsv("dayTwoinput.csv", "\t");
 $inputCheckSum = new \DayTwo\Checksum($array);
-echo "\nCheckSum: ".$inputCheckSum->checkSum();
+echo "\nCheckSum: ".$inputCheckSum->checkSum()."\n";
 
 /********************DAY TWO-B******************/
 $inputEvenlyDiv = new \DayTwo\EvenlyDiv($array);
-echo "\nEvenlyDiv: " . $inputEvenlyDiv->evenlyDiv();
+echo "EvenlyDiv: " . $inputEvenlyDiv->evenlyDiv()."\n";
 
+/********************DAY THREE******************/
+$input = 361527;
+$manhattan = new \DayThree\ManhattanDistance($input);
+echo "ManhattanDistance: ".$manhattan->getShortestPath()."\n";
--- sandbox.php
+++ sandbox.php
@@ -1,20 +1,8 @@
 <?php
-$arr = [3,8,6,5];
-$size = sizeof($arr);
-rsort($arr);
 
-// print_r($arr);
-for($i=0; $i < $size -1  ; $i++){
-    for ($j = 0; $j < $size; $j++) {
-        if($i == $j || $i-$j == 1){
-        }else{
-            if($arr[$i] % $arr[$j] == 0){
-                $result =  ($arr[$i] / $arr[$j]);
-                break;
-            }
-        }        
-    }
+$n = 3;
+$start = (int)($n/2);
+
+for($i= $start;$i < $n ; $i++){
+    echo $i."\n";
 }
-
-echo $result;
-
